f824031794593b946d1e271bec7cc2e20947288b
pablolobat0 <plobatorey@gmail.com>
2024-03-02 20:00:16 +0100

    Implementada estructura de datos y tabla de símbolos con las operaciones básicas

---
+++ tabla_de_simbolos.h
@@ -0,0 +1,12 @@
+#ifndef INC_01_TABLA_DE_SIMBOLOS_H
+#define INC_01_TABLA_DE_SIMBOLOS_H
+
+#include "definiciones.h"
+
+void crear_tabla_de_simbolos();
+
+int buscar_e_insertar_en_tabla_de_simbolos(char *lexema);
+
+void destruir_tabla_de_simbolos();
+
+#endif //INC_01_TABLA_DE_SIMBOLOS_H
