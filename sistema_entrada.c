096f066e6be4575e621090c9297197f7f8e7e415
pablolobat0 <plobatorey@gmail.com>
2024-03-07 18:50:14 +0100

    Merge de la rama feature/doble-buffer en main, resueltos los conflictos en analizador_lexico.c

--- sistema_entrada.c
+++ sistema_entrada.c
@@ -3,56 +3,87 @@
 #include <string.h>
 #include "sistema_entrada.h"
 
-#define TAM 64
+#define TAM_BUFFER 256
 
-typedef struct {
-    char lexema[TAM];
-    int longitud;
-} Buffer;
-
-Buffer buffer = {"", 0};
-
-FILE* archivo_fuente;
+char buffer[TAM_BUFFER];
+int inicio, delantero;
 
 
-void iniciar_sistema_entrada(char* nombre_archivo) {
+FILE *archivo_fuente;
+
+
+size_t cargar_bloque(char *bloque) {
+    return fread(bloque, sizeof(char), (TAM_BUFFER / 2) - 1, archivo_fuente);
+}
+
+void iniciar_sistema_entrada(char *nombre_archivo) {
     archivo_fuente = fopen(nombre_archivo, "r");
     if (archivo_fuente == NULL) {
         perror("Error al abrir el archivo.\n");
         exit(EXIT_FAILURE);
     }
+    // Inicializacion del doble buffer
+    inicio = 0;
+    delantero = 0;
+    buffer[(TAM_BUFFER / 2) - 1] = EOF;
+    buffer[TAM_BUFFER - 1] = EOF;
+    cargar_bloque(buffer);
 }
 
 int siguiente_caracter() {
-    int caracter = fgetc(archivo_fuente);
-    buffer.lexema[buffer.longitud] = (char) caracter;
-    buffer.longitud += 1;
-    return caracter;
-}
+    delantero++;
+    size_t elementos_leidos;
+    if (buffer[delantero] == EOF) {
+        switch (delantero) {
+            case (TAM_BUFFER / 2) - 1: // Final bloque A
+                elementos_leidos = cargar_bloque(buffer + (TAM_BUFFER / 2));
+                if (elementos_leidos < (TAM_BUFFER / 2) - 1) {
+                    buffer[elementos_leidos + (TAM_BUFFER / 2)] = EOF;
+                }
+                delantero++;
+                return buffer[delantero - 2];
+            case TAM_BUFFER - 1: // Final bloque B
+                elementos_leidos = cargar_bloque(buffer);
+                if (elementos_leidos < (TAM_BUFFER / 2) - 1) {
+                    buffer[elementos_leidos] = EOF;
+                }
+                delantero = 0;
+                return buffer[TAM_BUFFER - 2];
+        }
+    }
 
-void borrar_buffer() {
-    memset(buffer.lexema, 0, sizeof(buffer.lexema));
-    buffer.longitud = 0;
+    return buffer[delantero - 1];
 }
 
 char *get_lexema() {
     // Reservamos memoria para el lexema y el \0 final
-    char *lexema = (char*) malloc(sizeof(char ) * (buffer.longitud + 1));
-    for (int i = 0; i < buffer.longitud; i++) {
-        lexema[i] = buffer.lexema[i];
+    int longitud = 0;
+    if (inicio > delantero) {
+        longitud = TAM_BUFFER - 2 - inicio + delantero;
+    } else {
+        longitud = delantero - inicio;
     }
-    lexema[buffer.longitud] = '\0';
-    borrar_buffer();
+    char *lexema = (char *) malloc(sizeof(char) * (longitud + 1));
+    if (lexema == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < longitud; i++) {
+        lexema[i] = (char) buffer[inicio];
+        inicio++;
+        if (buffer[inicio] == EOF) { // Saltamos el final de bloque
+            inicio++;
+        }
+    }
+    lexema[longitud] = '\0';
     return lexema;
 }
 
 void retroceder_caracter() {
-    buffer.longitud -= 1;
-    fseek(archivo_fuente, -1, SEEK_CUR);
+    delantero -= 1;
 }
 
 void saltar_caracter() {
-    borrar_buffer();
+    inicio = delantero;
 }
 
 void cerrar_sistema_de_entrada() {
