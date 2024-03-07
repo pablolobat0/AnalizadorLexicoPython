55b70cd59e79ed9c865f9319ebeaf6ebec776a26
pablolobat0 <plobatorey@gmail.com>
2024-03-07 19:46:23 +0100

    Mejora del formato del analizador lexico

--- analizador_lexico.c
+++ analizador_lexico.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
+
 #include "analizador_lexico.h"
 #include "sistema_entrada.h"
 
@@ -16,33 +18,26 @@ void automata_alfanumerico() {
     } while (isalnum(caracter_actual) || caracter_actual == '_'); // Caracter alfanumerico o _
 }
 
-void automata_punto_flotante() {
-    int caracter_actual, componente_lexico_encontrado = 0;
-    int estado = 0;
-    while (!componente_lexico_encontrado) {
-        switch (estado) {
-            case 0:
-                do {
-                    caracter_actual = siguiente_caracter();
-                } while (isdigit(caracter_actual) || caracter_actual == '_');
+int automata_numero() {
+    int caracter_actual;
+    do {
+        caracter_actual = siguiente_caracter();
+    } while (isdigit(caracter_actual) || caracter_actual == '_');
+    return caracter_actual;
+}
 
-                if (caracter_actual == 'e')
-                    estado = 1;
-                else
-                    componente_lexico_encontrado = 1;
-                break;
-            case 1:
-                caracter_actual = siguiente_caracter();
-                if (caracter_actual == '-' || isdigit(caracter_actual))
-                    estado = 2;
-                break;
-            case 2:
-                do {
-                    caracter_actual = siguiente_caracter();
-                } while (isdigit(caracter_actual) || caracter_actual == '_');
-                componente_lexico_encontrado = 1;
-        }
-    }
+void automata_exponente(int caracter_actual) {
+    if (caracter_actual != 'e')
+        return;
+    caracter_actual = siguiente_caracter();
+    if (caracter_actual != '-' || !isdigit(caracter_actual))
+        return;
+    automata_numero();
+}
+
+void automata_punto_flotante() {
+    int ultimo_caracter = automata_numero();
+    automata_exponente(ultimo_caracter);
 }
 
 void automata_binario() {
@@ -59,24 +54,24 @@ void automata_octal() {
     } while ((caracter_actual >= 48 && caracter_actual <= 55) || caracter_actual == '_'); // Numero del 0 al 7 o _
 }
 
+bool es_hexadecimal(int caracter) {
+    return ((caracter >= 48 && caracter <= 57) || (caracter >= 65 && caracter <= 70) ||
+            (caracter >= 97 && caracter <= 102) || caracter == '_');
+}
+
 void automata_hexadecimal() {
     int caracter_actual;
     do {
         caracter_actual = siguiente_caracter();
-    } while ((caracter_actual >= 48 && caracter_actual <= 57) || (caracter_actual >= 65 && caracter_actual <= 70) ||
-             (caracter_actual >= 97 && caracter_actual <= 102) ||
-             caracter_actual == '_'); // Numero del 0 al 9, letra de la 'A' a la 'F', letra de la 'a' a la 'f' o _
+    } while (es_hexadecimal(
+            caracter_actual)); // Numero del 0 al 9, letra de la 'A' a la 'F', letra de la 'a' a la 'f' o _
 }
 
 void automata_entero() {
-    int caracter_actual;
-    do {
-        caracter_actual = siguiente_caracter();
-    } while (isdigit(caracter_actual) || caracter_actual == '_');
+    int ultimo_caracter = automata_numero();
 
-    if (caracter_actual == '.') {
+    if (ultimo_caracter == '.')
         automata_punto_flotante();
-    }
 }
 
 void automata_numerico() {
@@ -128,74 +123,78 @@ ComponenteLexico aceptar_lexema_simbolo() {
     return componente_lexico;
 }
 
-// Entiendase doble los que pueden ser de la forma '*', '*=', '**', o '**='
-ComponenteLexico automata_operadores_delimitadores_dobles(int caracter_actual, int anterior_caracter) {
-    if (caracter_actual == '=') {
+// Entiendase triple los que pueden ser de la forma '*', '*=', '**', o '**='
+ComponenteLexico automata_delimitadores_triples(int caracter_actual, int anterior_caracter) {
+    if (caracter_actual == '=') { // Forma '*'
         return aceptar_lexema();
     } else if (anterior_caracter == caracter_actual) {
-        if (siguiente_caracter() == '=') {
+        if (siguiente_caracter() == '=') { // Forma '**='
             return aceptar_lexema();
-        } else {
+        } else { // Forma '**'
             retroceder_caracter();
             return aceptar_lexema();
         }
-    } else {
+    } else { // Forma '*'
         retroceder_caracter();
         return aceptar_lexema_simbolo();
     }
 }
 
-// Entiendase simple los que pueden ser de la forma '&' o '&='
-ComponenteLexico automata_operadores_delimitadores_simples(int caracter_actual) {
-    if (caracter_actual == '=') {
+// Entiendase doble los que pueden ser de la forma '&' o '&='
+ComponenteLexico automata_operadores_delimitadores_dobles(int caracter_actual) {
+    if (caracter_actual == '=') { // Forma '&='
         return aceptar_lexema();
-    } else {
+    } else { // Forma '&'
         retroceder_caracter();
         return aceptar_lexema_simbolo();
     }
 }
 
-ComponenteLexico automata_simbolos(int estado) {
+bool es_posible_operador_delimitador_doble(int caracter) {
+    return (caracter == ':' || caracter == '=' || caracter == '!' || caracter == '+' || caracter == '%' ||
+            caracter == '@' || caracter == '&' || caracter == '|' || caracter == '^');
+}
+
+bool es_posible_delimitador_triple(int caracter) {
+    return (caracter == '/' || caracter == '*' || caracter == '<' || caracter == '>');
+}
+
+// Entiendase simple como los que tienen la forma '{' solo
+bool es_simbolo_simple(int caracter) {
+    return (caracter == ';' || caracter == ',' || caracter == '[' || caracter == ']' || caracter == '(' ||
+            caracter == ')' || caracter == '{' || caracter == '}');
+}
+
+ComponenteLexico automata_simbolos(int caracter_anterior) {
     int caracter_actual = siguiente_caracter();
-    switch (estado) {
-        case '.':
-            if (isdigit(caracter_actual)) {
-                automata_punto_flotante();
-                retroceder_caracter(); // Se retrocede un caracter para que no quede sin analizar
-                return aceptar_lexema();
-            } else {
-                retroceder_caracter();
-                return aceptar_lexema_simbolo();
-            }
-            break;
-        case '/':
-        case '*':
-        case '<':
-        case '>':
-            return automata_operadores_delimitadores_dobles(caracter_actual, estado);
-        case ':':
-        case '=':
-        case '!':
-        case '+':
-        case '%':
-        case '@':
-        case '&':
-        case '|':
-        case '^':
-            return automata_operadores_delimitadores_simples(caracter_actual);
-        case '-':
-            if (caracter_actual == '=' || caracter_actual == '>') {
-                return aceptar_lexema();
-            } else {
-                retroceder_caracter();
-                return aceptar_lexema_simbolo();
-            }
+
+    if (es_posible_delimitador_triple(caracter_anterior)) {
+        return automata_delimitadores_triples(caracter_actual, caracter_anterior);
+    } else if (es_posible_operador_delimitador_doble(caracter_anterior)) {
+        return automata_operadores_delimitadores_dobles(caracter_actual);
+    } else if (caracter_anterior == '.') {
+        if (isdigit(caracter_actual)) {
+            automata_punto_flotante();
+            retroceder_caracter(); // Se retrocede un caracter para que no quede sin analizar
+            return aceptar_lexema();
+        } else {
+            retroceder_caracter();
+            return aceptar_lexema_simbolo();
+        }
+    } else if (caracter_anterior == '-') { // Es un caso a parte porque puede tener las formas '-=' o '->'
+        if (caracter_actual == '=' || caracter_actual == '>') {
+            return aceptar_lexema();
+        } else {
+            retroceder_caracter();
+            return aceptar_lexema_simbolo();
+        }
     }
 }
 
 ComponenteLexico siguiente_componente_lexico() {
     int caracter_actual;
     ComponenteLexico componente_lexico;
+
     int componente_lexico_encontrado = 0;
     while (!componente_lexico_encontrado) {
         caracter_actual = siguiente_caracter();
@@ -205,47 +204,25 @@ ComponenteLexico siguiente_componente_lexico() {
         } else if (isdigit(caracter_actual)) {
             automata_numerico();
             componente_lexico_encontrado = 1;
+        } else if (es_simbolo_simple(caracter_actual)) {
+            return aceptar_lexema_simbolo(); // Cuando es un simbolo se devuelve el ASCII
+        } else if (es_posible_operador_delimitador_doble(caracter_actual) ||
+                   es_posible_delimitador_triple(caracter_actual) || caracter_actual == '.' || caracter_actual == '-') {
+            return automata_simbolos(caracter_actual);
+        } else if (caracter_actual == '#') {
+            saltar_linea();
+        } else if (caracter_actual == EOF) {
+            componente_lexico.lexema = NULL;
+            componente_lexico.componente_lexico = EOF;
+            cerrar_sistema_de_entrada();
+            return componente_lexico;
         } else {
-            switch (caracter_actual) {
-                case EOF:
-                    componente_lexico.lexema = NULL;
-                    componente_lexico.componente_lexico = EOF;
-                    cerrar_sistema_de_entrada();
-                    return componente_lexico;
-                case '#': // Comentarios de una linea
-                    saltar_linea();
-                    break;
-                case ';':
-                case ',':
-                case '[':
-                case ']':
-                case '(':
-                case ')':
-                case '{':
-                case '}':
-                    return aceptar_lexema_simbolo(); // Cuando es un simbolo se devuelve el ASCII
-                case '.':
-                case '/':
-                case '*':
-                case '<':
-                case '>':
-                case ':':
-                case '=':
-                case '!':
-                case '-':
-                case '+':
-                case '%':
-                case '@':
-                case '&':
-                case '|':
-                case '^':
-                    return automata_simbolos(caracter_actual);
-                default: // Si no es ninguna opcion, el caracter no tiene porque analizarse
-                    saltar_caracter();
-            }
+            saltar_caracter();
         }
     }
+
     retroceder_caracter(); // Se retrocede un caracter para que no quede sin analizar
+
     return aceptar_lexema();
 }
 
